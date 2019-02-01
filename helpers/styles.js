25bfa0ae666624ad6b9be814caadf5a3fbdbd82d
Seth McClaine <smcclaine@sans.org>
2019-02-01 09:08:52 -0700

    move check for conent.length outside of map (it doesn't get hit there..

--- helpers/styles.js
+++ helpers/styles.js
@@ -1,23 +1,25 @@
 const R = require('ramda');
 
 const styles = (content, type) => {
-    if(R.type(content) !== 'Array') {
+    if (R.type(content) !== 'Array') {
         console.log('@TODO - why isn\'t content an array?')
         content = [content];
+    }
+
+    if (!content.length) {
+        return [{
+            data: {},
+            marks: [{ type }],
+            value: '',
+            nodeType: 'text'
+        }];
     }
 
     return R.map((node) => {
         if(node.nodeType === 'text') {
             return R.assoc('marks', R.append({ type }, node.marks), node);
-        } else if (node.content.length) {
-            return R.assoc('content', styles(node.content, type), node);
         } else {
-            return [{
-                data: {},
-                marks: [{ type }],
-                value: '',
-                nodeType: 'text'
-            }];
+            return R.assoc('content', styles(node.content, type), node);
         }
     }, content);
 }
